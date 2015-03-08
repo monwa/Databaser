8e6912cea1e9ed56726d2338abc7b82810555202
Øystein Meland <oysmeland@gmail.com>
2015-03-08 15:33:55 +0100

    1. Utkast

--- Kalender.sql
+++ Kalender.sql
@@ -8,5 +8,27 @@ CREATE TABLE Gruppe
 CREATE TABLE Ansatt
 	(Brukernavn VARCHAR(20) NOT NULL, 
      Passord VARCHAR(40) NOT NULL, 
-     PRIMARY KEY (Brukernavn)
+     PRIMARY KEY (Brukernavn),
+     UNIQUE (Brukernavn)
      );
+     
+CREATE TABLE Avtale
+	(AvtaleID INT NOT NULL, 
+    Dato VARCHAR (11) NOT NULL, 
+    StartDATO VARCHAR(11) NOT NULL,
+    SluttDATO VARCHAR(11) NOT NULL,
+    Beskrivelse VARCHAR(225) NOT NULL,
+    PRIMARY KEY (AvtaleID)
+	);
+
+CREATE TABLE Alarm
+	(AlarmID INT NOT NULL,
+    Tidspunkt VARCHAR(5) NOT NULL,
+    AlarmType VARCHAR(25) NOT NULL,
+    PRIMARY KEY (AlarmID), FOREIGN KEY (AvtaleID) REFERENCES Avtale );
+
+CREATE TABLE Rom
+	(RomID INT NOT NULL,
+	Kapasitet INT NOT NULL,
+    PRIMARY KEY (RomID)
+    );
